28e71ff1583624f3538c73adc154a0821fc97bea
Winsome Yuen <winsomeyuen@hotmail.com>
2020-04-14 03:06:14 +0100

    Change params for handlers

--- src/Handler/getResponse.jsx
+++ src/Handler/getResponse.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export default function getResponse(xappToken){
-	axios.get('https://api.artsy.net/api/v1/artists/popular', {
+	axios.get('https://api.artsy.net/api/artworks', {
        headers: {
          'X-Access-Token': xappToken,
-         'Accept': 'application/json',
+         'Accept': 'application/vnd.artsy-v2+json'
          }
      })
      .then(function (response) {
--- src/Handler/getToken.jsx
+++ src/Handler/getToken.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 export default async function getToken(){
    try {
      const response = await axios.post('https://api.artsy.net/api/tokens/xapp_token', {
-         client_id: 'fed4d28c66b2c73d5cf8',
-         client_secret: 'e900abcbe5a15f70322d64f2186c9f94'
+         client_id: 'f98b1153b8ea5c90b480',
+         client_secret: '942e0a60f9da772cc528f8024a610dbf'
      });
      return  response.data.token;
    } catch (e) {
