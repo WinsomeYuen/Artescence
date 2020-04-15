6d0118165ce29fa996496cb4fd457781625e1662
Winsome Yuen <winsomeyuen@hotmail.com>
2020-04-15 01:03:32 +0100

    Implementation for Gallery API
    MET API

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import getResponse from '../Handler/getResponse.jsx'
-import getToken from '../Handler/getToken.jsx'
+import React from 'react';
+import getResponse from '../Handler/getResponse.jsx';
 import './Button.css';
 
 const Button = ({selectedIcon}) => {
-	const [selected] = selectedIcon;
-   const [xappToken, setXappToken] = useState(null);
+	const selected = selectedIcon;
+   /**const [xappToken, setXappToken] = useState(null);
    useEffect(() => {
      getToken().then(token => setXappToken(token))
-   }, [])
+   }, [])**/
 
    return(
       <div className="buttonBlock">
          <span className="btn">
-            <input className="btn-outline" type="button" value="Press Me" onClick={ () => getResponse(xappToken) }/>
+            <input className="btn-outline" type="button" value="Press Me" onClick={ () => getResponse(selected) }/>
          </span>
       </div>
    );
--- src/Handler/getResponse.jsx
+++ src/Handler/getResponse.jsx
@@ -1,19 +1,28 @@
+import React from 'react';
 import axios from 'axios';
 
-export default function getResponse(xappToken){
-	axios.get('https://api.artsy.net/api/artworks', {
-       headers: {
-         'X-Access-Token': xappToken,
-         'Accept': 'application/vnd.artsy-v2+json'
-         }
-     })
-     .then(function (response) {
-       console.log(response);
-     })
-     .catch(function (error) {
-       console.log(error);
-     })
-     .then(function () {
-       // always executed
-     });
+export default function getResponse(selected){
+   if(selected === "gallery"){
+      getGallery();
+   }
+}
+
+async function getGallery(){
+	try {
+     const artist = randomObject(['Auguste Renoir','Pablo Picasso','Vincent van Gogh','Andy Warhol',
+      'Bridget Riley','Claude Monet','Henri Matisse','Georgia OKeeffe', 'Cindy Sherman' ]);
+     const response = await axios.get('https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q='+ artist);
+     const artwork = await axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/'+ randomObject(response.data.objectIDs));
+	  console.log(artwork);
+	  return(
+           <div>Boo</div>
+        );
+   } catch (e) {
+     console.log(`😱 Axios request failed: ${e}`);
+     return "Error";
+   }
+}
+
+function randomObject(array) {
+  return array[Math.floor(Math.random() * array.length)];
 }
