6870dcc7f115398b7f5898967bd8ea79e1a61470
Winsome Yuen <winsomeyuen@hotmail.com>
2020-04-19 02:00:18 +0100

    Use Google and Wikipedia API
    swapped the met api

--- src/Handler/getResponse.jsx
+++ src/Handler/getResponse.jsx
@@ -5,7 +5,7 @@ export default function GetResponse(selected){
 	const [response, setResponse] = useState(null);
 
 	useEffect(() => {
-        getGallery().then(object => setResponse(object.data));
+        getGallery().then(object => setResponse(object));
    }, [])
 
    return response;
@@ -13,11 +13,49 @@ export default function GetResponse(selected){
 
 async function getGallery(){
 	try {
-     const artist = randomObject(['Auguste Renoir','Pablo Picasso','Vincent van Gogh','Andy Warhol',
-      'Bridget Riley','Claude Monet','Henri Matisse', 'Cindy Sherman' ]);
-     const response = await axios.get('https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q='+ artist);
-     const artwork = await axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/'+ randomObject(response.data.objectIDs));
-      return artwork;
+	  const artist = 'Painting by ' + randomObject(['Vincent van Gogh','Henri Matisse']);
+     const response = await axios.get('https://kgsearch.googleapis.com/v1/entities:search?key=AIzaSyC5uAjNixQGFWTpQ4nXj1379MGIGYBD6c0&types=VisualArtwork&query='+ artist);
+     const itemList = response.data.itemListElement;
+
+     var paintingsByArtist = [];
+     itemList.forEach(function (item) {
+         if(item.result.description === artist && item.result.hasOwnProperty("detailedDescription")){ paintingsByArtist.push(item) }
+     });
+
+     const painting = randomObject(paintingsByArtist);
+     var image = "";
+     if(!painting.result.image){ getWiki(painting.result.name).then(object => painting['image'] = object) }
+
+     return painting;
+   } catch (e) {
+     console.log(`😱 Axios request failed: ${e}`);
+     return "Error";
+   }
+}
+
+async function getWiki(painting){
+	try {
+     const response = await axios.get('https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=images&format=json&titles=' + painting);
+     const pages = response.data.query.pages;
+     if(!pages){return ""}
+     var imageFile = '';
+     for (var page in pages) {
+        for (var image of pages[page].images) {
+            imageFile = image.title;
+            break;
+        }
+     }
+
+     const imageInfo = await axios.get('https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=imageinfo&iiprop=url&format=json&titles=' + imageFile);
+	  const imagePages = imageInfo.data.query.pages;
+	  if(!imagePages){return ""};
+	  var image = '';
+	  for (var p in imagePages) {
+			image = imagePages[p].imageinfo[0].url;
+			break;
+     }
+
+     return image;
    } catch (e) {
      console.log(`😱 Axios request failed: ${e}`);
      return "Error";
