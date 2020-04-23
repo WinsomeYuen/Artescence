6e4ac39bdb2ee3fa206f26d84ccb4474aea0e028
Winsome Yuen <winsomeyuen@hotmail.com>
2020-04-23 02:50:01 +0100

    Adjust order and styling of modal
    Changed order of divs

--- src/Handler/setGalleryResponse.jsx
+++ src/Handler/setGalleryResponse.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 
-export default function setGalleryResponse(response){
+export default function setGalleryResponse(response, close){
    return(
       <div>
-          <h1>{response.result.name}</h1>
-          <h5>{response.result.description}</h5>
-          {response.image === "Error" &&(
-              <p className="text">{response.result.detailedDescription.articleBody}</p>
-          )}
-          {response.image && response.image !== "Error" &&(
-            <div className="container">
-               <div className="left">{response.result.detailedDescription.articleBody}</div>
-               <div className="right"><img className="image" alt="object" src={response.image}/></div>
-            </div>
-          )}
-          {response.result.image &&(
-            <div className="container">
-               <div className="left">{response.result.detailedDescription.articleBody}</div>
-               <div className="right"><img className="image" alt="object" src={response.result.image.contentUrl}/></div>
-            </div>
-          )}
+         <div className="modal-header">
+	          <div className="close-modal" onClick={ () => { close(false) }} >&#10006;</div>
+	          <h1>{response.result.name}</h1>
+	          <h5>{response.result.description}</h5>
+         </div>
+         <div>
+	          {response.image === "Error" &&(
+	            <div className="modal-body">
+	              <p className="text">{response.result.detailedDescription.articleBody}</p>
+	             </div>
+	          )}
+	          {response.image && response.image !== "Error" &&(
+	            <div className="modal-body">
+	               <div className="left"><span className="modal-text">{response.result.detailedDescription.articleBody}</span></div>
+	               <div className="right"><img className="image" alt="object" src={response.image}/></div>
+	            </div>
+	          )}
+	          {response.result.image &&(
+	            <div className="modal-body">
+	               <div className="left"><span className="modal-text">{response.result.detailedDescription.articleBody}</span></div>
+	               <div className="right"><img className="image" alt="object" src={response.result.image.contentUrl}/></div>
+	            </div>
+	          )}
+          </div>
        </div>
    )
 }
--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -3,22 +3,22 @@ import getResponse from '../Handler/getResponse.jsx';
 import setGalleryResponse from '../Handler/setGalleryResponse.jsx';
 import setMuseumResponse from '../Handler/setMuseumResponse.jsx';
 import setExploreResponse from '../Handler/setExploreResponse.jsx';
+import image from '../assets/scrolling-background.png';
 import Spinner from '../Spinner/Spinner.jsx';
 
 import './Modal.css';
 
  const Modal = ({close, item}) => {
 	const response = getResponse(item);
-	//console.log(response);
 
 	return (
 		<div>
 		   {response === null &&(<Spinner/>)}
 			{response &&(
-				<div className='modal'>
-	            <div className='popup'>
-	            	 <button onClick={ () => { close(false) }}>close me</button>
-	                {item === "gallery" && (setGalleryResponse(response) )}
+				<div className='modal' style={{backgroundImage: `url(${image})`}}>
+					<div className="modal-sandbox"></div>
+	            <div className='modal-box'>
+	                {item === "gallery" && (setGalleryResponse(response, close) )}
 	                {item === "museum" && (setMuseumResponse(response) )}
 	                {item === "explore" && (setExploreResponse(response) )}
 	            </div>
